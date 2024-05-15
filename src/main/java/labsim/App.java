3c3b9d9c7319289dcd401e6c3eef814a49f2705f
matrero99 <ezzemyllo@gmail.com>
2024-05-15 17:41:01 -0300

    v 3.0.3

--- src/main/java/labsim/App.java
+++ src/main/java/labsim/App.java
@@ -1,20 +1,37 @@
 package labsim;
 
-import javax.swing.SwingUtilities;
+import labsim.motor.Motor;
+import labsim.motor.Simulacion;
+import labsim.politicas.MultiplesServidores;
+import labsim.recursos.CreadorServidores;
+import labsim.recursos.CustomRandomizer;
+import labsim.recursos.Estadisticas;
+import labsim.recursos.EstadisticasGenerales;
 /**
  * Clase principal.
  */
 public class App {
 
+    private static final float TAMANIOSIMULACION = 40320;
 
     /**
      * Funcion Principal (main)
      * @param args
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            SimulacionGUI gui = new SimulacionGUI();
-            gui.setVisible(true);
-        });
+        int i = 0;
+        EstadisticasGenerales estadisticasGenerales = new EstadisticasGenerales();
+        // Ejecuta 50 veces la simulacion con distintas semillas
+        while (i<50) {
+            Estadisticas estadisticas = new Estadisticas();
+            Motor motor = new Simulacion(TAMANIOSIMULACION, CreadorServidores.nServidoresUnicasColas(2), estadisticas, new CustomRandomizer(), new MultiplesServidores());    
+            motor.correr();
+            i++;
+            System.out.println("\nEjecucion Numero: "+ i +"\n\n");
+            System.out.println(estadisticas.toString(TAMANIOSIMULACION)+"\n"); 
+            estadisticasGenerales.recolectarDatos(estadisticas);
+        }
+        estadisticasGenerales.calcularEstadisticasGeneral();
+        System.out.println(estadisticasGenerales.toString());
     }
 }
--- src/main/java/labsim/recursos/EstadisticasGenerales.java
+++ src/main/java/labsim/recursos/EstadisticasGenerales.java
@@ -23,6 +23,8 @@ public class EstadisticasGenerales {
     private List<Double> ocioMax;
     private List<Double> colaMin;
     private List<Double> colaMax;
+    private List<Double> servidor1;
+    private List<Double> servidor2;
     
     private double mediaArribados;
     private double mediaAterrizados;
@@ -37,6 +39,8 @@ public class EstadisticasGenerales {
     private double mediaOcioMax;
     private double mediaColaMin;
     private double mediaColaMax;
+    private double mediaServidor1;
+    private double mediaServidor2;
 
     private double desviacionArribados;
     private double desviacionAterrizados;
@@ -50,9 +54,13 @@ public class EstadisticasGenerales {
     private double desviacionOcioMin;
     private double desviacionOcioMax;    
     private double desviacionColaMin;
-    private double desviacionColaMax;
+    private double desviacionColaMax;  
+    private double desviacionServidor1;
+    private double desviacionServidor2;
     
-
+    /**
+     * Constructor de la Estadistica General.
+     */
     public EstadisticasGenerales() {
         avionesArribados = new ArrayList<>();
         avionesAterrizados = new ArrayList<>();
@@ -67,61 +75,136 @@ public class EstadisticasGenerales {
         ocioMax = new ArrayList<>();
         colaMin = new ArrayList<>();
         colaMax = new ArrayList<>();
+        servidor1 = new ArrayList<>();
+        servidor2 = new ArrayList<>();
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de Aviones Arribados de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesArribados(double cantidad) {
         avionesArribados.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de Aviones Aterrizados de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesAterrizados(double cantidad) {
         avionesAterrizados.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Transito Total de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesTransitoTotal(double cantidad) {
         transitoTotal.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Transito Minimo de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesTransitoMin(double cantidad) {
         transitoMin.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Transito Maximo de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesTransitoMax(double cantidad) {
         transitoMax.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Espera Total de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesEsperaTotal(double cantidad) {
         esperaTotal.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Espera Minimo de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesEsperaMin(double cantidad) {
         esperaMin.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Espera Maximo de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesEsperaMax(double cantidad) {
         esperaMax.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Ocio Total de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesOcioTotal(double cantidad) {
         ocioTotal.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Ocio Minimo de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesOcioMin(double cantidad) {
         ocioMin.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Ocio Maximo de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesOcioMax(double cantidad) {
         ocioMax.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Cola Minima de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesColaMin(double cantidad) {
         colaMin.add(cantidad);
     }
 
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Cola Maxima de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
     public void agregarAvionesColaMax(double cantidad) {
         colaMax.add(cantidad);
     }
+    
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Desgaste del Servidor 1 de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
+    public void agregarDesgasteServidor1(double cantidad) {
+        servidor1.add(cantidad);
+    }
 
-    // Método para calcular la desviación estándar de un conjunto de datos
+    /**
+     * Funcion que agrega cada resultado parcial de cantidad de Desgaste del Servidor 2 de una Ejecucion en un ArrayList.
+     * @param cantidad
+     */
+    public void agregarDesgasteServidor2(double cantidad) {
+        servidor2.add(cantidad);
+    }
+
+    /**
+     * Funcion que calcula la desviación estándar de un conjunto de datos.
+     * @param datos
+     * @param media
+     * @return desviacion
+     */
     public double calcularDesviacion(List<Double> datos, double media) {
         if (datos.isEmpty()) {
             return 0; // Manejar caso especial si no hay datos
@@ -139,7 +222,11 @@ public class EstadisticasGenerales {
         return desviacion;
     }
 
-    // Método auxiliar para calcular la media de una lista de enteros
+    /**
+     * Funcion auxiliar para calcular la media de una lista.
+     * @param lista
+     * @return
+     */
     private double calcularMedia(List<Double> lista) {
         int suma = 0;
         for (double cantidad : lista) {
@@ -148,6 +235,9 @@ public class EstadisticasGenerales {
         return (double) suma / EJECUCIONES;
     }
 
+    /**
+     * Funcion que realiza el calculo de las estadísticas generales para varios conjuntos de datos.
+     */
     public void calcularEstadisticasGeneral(){
         this.mediaArribados = calcularMedia(this.avionesArribados);
         this.desviacionArribados = calcularDesviacion(this.avionesArribados, this.mediaArribados);
@@ -175,24 +265,58 @@ public class EstadisticasGenerales {
         this.desviacionColaMin = calcularDesviacion(this.colaMin, this.mediaColaMin);
         this.mediaColaMax = calcularMedia(this.colaMax);
         this.desviacionColaMax = calcularDesviacion(this.colaMax, this.mediaColaMax);
+        this.mediaServidor1 = calcularMedia(this.servidor1);
+        this.desviacionServidor1 = calcularDesviacion(this.servidor1, this.mediaServidor1);
+        this.mediaServidor2 = calcularMedia(this.servidor2);
+        this.desviacionServidor2 = calcularDesviacion(this.servidor2, this.mediaServidor2);
     }
 
+    /**
+     * Funcion que recolecta las Estadisticas de cada Ejecucion.
+     * @param estadisticas
+     */
+    public void recolectarDatos(Estadisticas estadisticas) {
+        agregarAvionesArribados(estadisticas.getCantidadAvionesArribados());
+        agregarAvionesAterrizados(estadisticas.getCantidadAvionesAterrizados());
+        agregarAvionesTransitoTotal(estadisticas.getTiempoTransito());
+        agregarAvionesTransitoMin(estadisticas.getTiempoMinTransito());
+        agregarAvionesTransitoMax(estadisticas.getTiempoMaxTransito());
+        agregarAvionesEsperaTotal(estadisticas.getTiempoEspera());
+        agregarAvionesEsperaMin(estadisticas.getTiempoMinEspera());
+        agregarAvionesEsperaMax(estadisticas.getTiempoMaxEspera());
+        agregarAvionesOcioTotal(estadisticas.getTiempoOcio());
+        agregarAvionesOcioMin(estadisticas.getTiempoMinOcio());
+        agregarAvionesOcioMax(estadisticas.getTiempoMaxOcio());
+        agregarAvionesColaMin(estadisticas.getMinCola());
+        agregarAvionesColaMax(estadisticas.getMaxCola());
+        agregarDesgasteServidor1(estadisticas.getServidorDesgaste(0));
+        agregarDesgasteServidor2(estadisticas.getServidorDesgaste(1));
+    }
+
+    /**
+     * Funcion que retorna los datos en un intervalo con una certeza del 95%.
+     */
     @Override
     public String toString() {
         double raiz = Math.sqrt(EJECUCIONES);
-        return "Estadisticas Generales para "+EJECUCIONES+" ejecuciones con el 95% de certeza"
-                +"\nAviones Arribados = (" + (mediaArribados - (1.96*(desviacionArribados/raiz))) +" , " + (mediaArribados + (1.96*(desviacionArribados/raiz))) +")"
-                +"\nAviones Aterrizados = (" + (mediaAterrizados - (1.96*(desviacionAterrizados/raiz))) +" , " + (mediaAterrizados + (1.96*(desviacionAterrizados/raiz))) +")"
-                +"\nTotal de Transito = (" + (mediaTransitoTotal - (1.96*(desviacionTransitoTotal/raiz))) +" , " + (mediaTransitoTotal + (1.96*(desviacionTransitoTotal/raiz))) +")"
-                +"\nTotal de Transito Minimo = (" + (mediaTransitoMin - (1.96*(desviacionTransitoMin/raiz))) +" , " + (mediaTransitoMin + (1.96*(desviacionTransitoMin/raiz))) +")"
-                +"\nTotal de Transito Maximo = (" + (mediaTransitoMax - (1.96*(desviacionTransitoMax/raiz))) +" , " + (mediaTransitoMax + (1.96*(desviacionTransitoMax/raiz))) +")"
-                +"\nTotal de Espera = (" + (mediaEsperaTotal - (1.96*(desviacionEsperaTotal/raiz))) +" , " + (mediaEsperaTotal + (1.96*(desviacionEsperaTotal/raiz))) +")"
-                +"\nTotal de Espera Minimo = (" + (mediaEsperaMin - (1.96*(desviacionEsperaMin/raiz))) +" , " + (mediaEsperaMin + (1.96*(desviacionEsperaMin/raiz))) +")"
-                +"\nTotal de Espera Maximo = (" + (mediaEsperaMax - (1.96*(desviacionEsperaMax/raiz))) +" , " + (mediaEsperaMax + (1.96*(desviacionEsperaMax/raiz))) +")"
-                +"\nTotal de Ocio = (" + (mediaOcioTotal - (1.96*(desviacionOcioTotal/raiz))) +" , " + (mediaOcioTotal + (1.96*(desviacionOcioTotal/raiz))) +")"
-                +"\nTotal de Ocio Minimo = (" + (mediaOcioMin - (1.96*(desviacionOcioMin/raiz))) +" , " + (mediaOcioMin + (1.96*(desviacionOcioMin/raiz))) +")"
-                +"\nTotal de Ocio Maximo = (" + (mediaOcioMax - (1.96*(desviacionOcioMax/raiz))) +" , " + (mediaOcioMax + (1.96*(desviacionOcioMax/raiz))) +")"
-                +"\nTotal de Cola Minimo = (" + (mediaColaMin - (1.96*(desviacionColaMin/raiz))) +" , " + (mediaColaMin + (1.96*(desviacionColaMin/raiz))) +")"
-                +"\nTotal de Cola Maximo = (" + (mediaColaMax - (1.96*(desviacionColaMax/raiz))) +" , " + (mediaColaMax + (1.96*(desviacionColaMax/raiz))) +")";
-    }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Estadísticas Generales para ").append(EJECUCIONES).append(" ejecuciones con el 95% de certeza\n")
+        .append("----------------------------------------------------\n")
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Aviones Arribados", mediaArribados - (1.96 * (desviacionArribados / raiz)), mediaArribados + (1.96 * (desviacionArribados / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Aviones Aterrizados", mediaAterrizados - (1.96 * (desviacionAterrizados / raiz)), mediaAterrizados + (1.96 * (desviacionAterrizados / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Tránsito", mediaTransitoTotal - (1.96 * (desviacionTransitoTotal / raiz)), mediaTransitoTotal + (1.96 * (desviacionTransitoTotal / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Tránsito Mínimo", mediaTransitoMin - (1.96 * (desviacionTransitoMin / raiz)), mediaTransitoMin + (1.96 * (desviacionTransitoMin / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Tránsito Máximo", mediaTransitoMax - (1.96 * (desviacionTransitoMax / raiz)), mediaTransitoMax + (1.96 * (desviacionTransitoMax / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Espera", mediaEsperaTotal - (1.96 * (desviacionEsperaTotal / raiz)), mediaEsperaTotal + (1.96 * (desviacionEsperaTotal / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Espera Mínimo", mediaEsperaMin - (1.96 * (desviacionEsperaMin / raiz)), mediaEsperaMin + (1.96 * (desviacionEsperaMin / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Espera Máximo", mediaEsperaMax - (1.96 * (desviacionEsperaMax / raiz)), mediaEsperaMax + (1.96 * (desviacionEsperaMax / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Ocio", mediaOcioTotal - (1.96 * (desviacionOcioTotal / raiz)), mediaOcioTotal + (1.96 * (desviacionOcioTotal / raiz))))
+        .append(String.format("%-30s: (%.5f , %.5f)\n", "Total de Ocio Mínimo", mediaOcioMin - (1.96 * (desviacionOcioMin / raiz)), mediaOcioMin + (1.96 * (desviacionOcioMin / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Ocio Máximo", mediaOcioMax - (1.96 * (desviacionOcioMax / raiz)), mediaOcioMax + (1.96 * (desviacionOcioMax / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Cola Mínimo", mediaColaMin - (1.96 * (desviacionColaMin / raiz)), mediaColaMin + (1.96 * (desviacionColaMin / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Cola Máximo", mediaColaMax - (1.96 * (desviacionColaMax / raiz)), mediaColaMax + (1.96 * (desviacionColaMax / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Desgaste Servidor 1", mediaServidor1 - (1.96 * (desviacionServidor1 / raiz)), mediaServidor1 + (1.96 * (desviacionServidor1 / raiz))))
+        .append(String.format("%-30s: (%.2f , %.2f)\n", "Total de Desgaste Servidor 2", mediaServidor2 - (1.96 * (desviacionServidor2 / raiz)), mediaServidor2 + (1.96 * (desviacionServidor2 / raiz))));
+        return sb.toString();
+    }    
 }
